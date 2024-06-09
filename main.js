92dc6208651575fc185791e8ea50f90de3e5b2d2
Andrey-6oyko <andreyboyko200032@gmail.com>
2024-06-09 17:09:41 +0300

    Second

--- main.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm_password').value;
-    if (password !== confirmPassword) {
-        alert('Паролі не співпадають. Спробуйте ще раз.');
-        return false;
-    }
-    return true;
-}
